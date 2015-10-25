0bd4ecacb16af3872cb115762c55a2eb40128690
Pavel Plevako <kaminapoke@gmail.com>
2015-10-25 22:14:43 +0300

    Add min length validation to article title.

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,10 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
 
 # Create your models here.
 class Article(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, validators=[MinLengthValidator(5)])
     text = models.TextField()
 
     def __unicode__(self):  # __str__ on Python 3
