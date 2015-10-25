da956c405c25d9934a88e7637e59b746d83e141c
Pavel Plevako <kaminapoke@gmail.com>
2015-10-25 19:46:44 +0300

    Change Article repr to a more meaningful value (title).

--- blog/models.py
+++ blog/models.py
@@ -6,9 +6,11 @@ class Article(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
 
+    def __unicode__(self):  # __str__ on Python 3
+        return self.title
+
 
 class Comment(models.Model):
     article = models.ForeignKey(Article)
     commenter = models.CharField(max_length=200)
     text = models.TextField()
-
