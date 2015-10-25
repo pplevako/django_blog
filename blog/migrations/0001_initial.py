20540096e8f52f7ca190febab7668748249b3d1e
Pavel Plevako <kaminapoke@gmail.com>
2015-10-25 19:09:37 +0300

    Create Article and Comment models and corresponding migrations.

---
+++ blog/migrations/0001_initial.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Article',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=200)),
+                ('text', models.TextField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Comment',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('commenter', models.CharField(max_length=200)),
+                ('text', models.TextField()),
+                ('article', models.ForeignKey(to='blog.Article')),
+            ],
+        ),
+    ]
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article)
+    commenter = models.CharField(max_length=200)
+    text = models.TextField()
+
