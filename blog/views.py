1f5899906cf24e180b8505d41e1273c4966762d2
Pavel Plevako <kaminapoke@gmail.com>
2015-10-25 19:51:16 +0300

    Add Index view.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from blog.models import Article
+
 
 # Create your views here.
+class IndexView(generic.ListView):
+    model = Article
+    # template_name = 'blog/article_list.html'
+    # context_object_name = 'article_list'
